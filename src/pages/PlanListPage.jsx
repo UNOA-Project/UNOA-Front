ccc53180a61eb2065bec3ed13e2b9c7286e16fe9
song_eun <dmswo546@gmail.com>
2025-06-14 14:41:31 +0900

    build: 프로젝트 초기 세팅

---
+++ src/pages/PlanListPage.jsx
@@ -0,0 +1,3 @@
+export default function PlanListPage() {
+  return <div>PlanListPage</div>
+}
---
+++ src/routes/index.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from 'react-router-dom'
+import MainPage from '@/pages/Mainpage'
+import ChatbotPage from '@/pages/ChatbotPage'
+import PlanListPage from '@/pages/PlanListPage'
+import MyPage from '@/pages/Mypage'
+import RegisterPage from '@/pages/RegisterPage'
+import LoginPage from '@/pages/LoginPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainPage />,
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/chatbot',
+    element: <ChatbotPage />,
+  },
+  {
+    path: '/planlist',
+    element: <PlanListPage />,
+  },
+  {
+    path: '/mypage',
+    element: <MyPage />,
+  },
+])
+
+export default router
