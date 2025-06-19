c669651092acd7456c52c15349424e1eaa6dcef4
Lacheln <psionict@naver.com>
2025-06-20 03:12:08 +0900

    chore: API연동에 맞는 변수명 변경

--- src/components/TabMenu/TabMenu.jsx
+++ src/components/TabMenu/TabMenu.jsx
@@ -3,10 +3,10 @@ import styles from './TabMenu.module.css'
 
 const TabMenu = ({ selectedCategory, onCategoryChange }) => {
   const tabs = [
-    { key: '5GLTE', label: '5G/LTE' },
-    { key: 'Online', label: '온라인 전용 요금제' },
-    { key: 'TabWatch', label: '태블릿/스마트워치' },
-    { key: 'Dual', label: '듀얼넘버 플러스' },
+    { key: '5G/LTE 요금제', label: '5G/LTE' },
+    { key: '온라인 다이렉트 요금제', label: '온라인 전용 요금제' },
+    { key: '태블릿/워치 요금제', label: '태블릿/스마트워치' },
+    { key: '듀얼심 요금제', label: '듀얼넘버 플러스' },
   ]
 
   return (
