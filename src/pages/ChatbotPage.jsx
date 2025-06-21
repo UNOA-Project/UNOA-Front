f134b98a861ea88884e09df53c63095a9a9482dd
song_eun <dmswo546@gmail.com>
2025-06-21 21:39:39 +0900

    style: 챗봇 페이지에 비눗방울 배경 이미지 추가
    - 선택/드래그 불가 처리
    - 최하단 z-index로 배치하여 콘텐츠와 겹치지 않도록 설정

--- src/pages/ChatbotPage.jsx
+++ src/pages/ChatbotPage.jsx
@@ -10,6 +10,8 @@ import ChatContainer from '../components/chatbot/ChatContainer'
 import WelcomeMessage from '../components/chatbot/WelcomeMessage'
 import LoadingScreen from '../components/chatbot/LoadingScreen'
 
+import SoapBubble from '@/assets/soap-bubble.svg?react'
+
 function ChatbotPage() {
   const [currentMode, setCurrentMode] = useState('normal')
   const { socket, isConnected } = useSocket()
@@ -31,12 +33,18 @@ function ChatbotPage() {
     // .appContainer 스타일 적용
     <div className="flex h-[calc(100vh-var(--header-height))] items-center justify-center bg-gradient-to-br from-[#899df4] to-[#9262c3]">
       {/* .welcomeMessageContainer 스타일 적용 (반응형) */}
-      <div className="hidden lg:block">
+      <div className="pointer-events-none fixed top-[var(--header-height)] left-0 z-0 h-[calc(100vh-var(--header-height))] w-[100vw] select-none">
+        <SoapBubble className="absolute top-[15%] left-[20%] h-40 w-40" />
+        <SoapBubble className="absolute top-[8%] right-[2%] h-80 w-80" />
+        <SoapBubble className="absolute -bottom-[1%] -left-[2%] h-40 w-40" />
+        <SoapBubble className="absolute bottom-[13%] left-[35%] h-20 w-20" />
+      </div>
+      <div className="z-10 hidden lg:block">
         <WelcomeMessage />
       </div>
 
       {/* .app 스타일 적용 (반응형) */}
-      <div className="flex h-full w-full max-w-[550px] flex-col bg-gradient-to-b from-[#F9F9FF] to-[#F7F2FF] shadow-none lg:h-[80vh] lg:max-h-[80vh] lg:max-w-[600px] lg:rounded-xl lg:shadow-[0_4px_15px_rgba(0,0,0,0.2)]">
+      <div className="z-10 flex h-full w-full max-w-[550px] flex-col bg-gradient-to-b from-[#F9F9FF] to-[#F7F2FF] shadow-none lg:h-[80vh] lg:max-h-[80vh] lg:max-w-[600px] lg:rounded-xl lg:shadow-[0_4px_15px_rgba(0,0,0,0.2)]">
         <ChatHeader
           currentMode={currentMode}
           onModeChange={handleModeChange}
